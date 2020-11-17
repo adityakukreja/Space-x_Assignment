65554d14670ec2a863b6e2d86fe9476408a1cfbe
adityakukreja <adityakukreja07@gmail.com>
2020-11-18 03:39:36 +0530

    changes

--- src/app/space-x-launch/output.model.ts
+++ src/app/space-x-launch/output.model.ts
@@ -3,7 +3,7 @@ import { Cores } from './cores.model';
 export class SpaceXDeatailsModel{
     flight_number : number;
     mission_name : string;
-    mission_id : string[];
+    mission_id : string[]=[];
     launch_year : string;
     launch_success : string;
     links = new Links;
--- src/app/space-x-launch/space-x-launch.component.ts
+++ src/app/space-x-launch/space-x-launch.component.ts
@@ -18,6 +18,7 @@ land_success : boolean;
 launchyear : string ="";
 launchsuccess : string ="";
 landsuccess : string ="";
+nullCheck : boolean = false;
 
   constructor(private service : spacexService) { }
 
@@ -29,6 +30,12 @@ landsuccess : string ="";
   getAllLaunchDetails(){
     this.service.getAllLaunchDetails("100").subscribe(
       res =>{
+        this.nullCheck=false;
+
+        if(res.length === 0){
+          this.listNullCheck();
+        }
+        if(res !== null){
 
         for(let val of res){
           const launchDetails = new SpaceXDeatailsModel;
@@ -37,12 +44,13 @@ landsuccess : string ="";
           launchDetails.launch_success = val.launch_success;
           launchDetails.links.mission_patch = val.links.mission_patch;
           launchDetails.launch_year = val.launch_year;
-          launchDetails.mission_id = val.mission_id;
+          for(let a of val.mission_id){
+            launchDetails.mission_id.push(a);
+          }
           launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
           this.launchDetailsList.push(launchDetails);
-          
         }
-
+      }
 
       },error =>{
       }
@@ -52,7 +60,12 @@ landsuccess : string ="";
   getLaunchSuccessDetails(launch_sucess_filter : string){
     this.service.getAllLaunch_SuccessDetails("100",launch_sucess_filter).subscribe(
       res =>{
+        this.nullCheck=false;
+        if(res.length === 0){
+          this.listNullCheck();
+        }
         this.launchDetailsList=[];
+        if(res !== null){
         for(let val of res){
           const launchDetails = new SpaceXDeatailsModel;
           launchDetails.mission_name = val.mission_name;
@@ -63,7 +76,8 @@ landsuccess : string ="";
           launchDetails.mission_id = val.mission_id;
           launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
           this.launchDetailsList.push(launchDetails);
-          
+
+        }
         }
 
       },error =>{
@@ -74,7 +88,12 @@ landsuccess : string ="";
   getLandSuccessDetails(land_sucess_filter : string){
     this.service.getAllLand_SuccessDetails("100",land_sucess_filter).subscribe(
       res =>{
+        this.nullCheck=false;
+        if(res.length === 0){
+          this.listNullCheck();
+        }
         this.launchDetailsList=[];
+        if(res !== null){
         for(let val of res){
           const launchDetails = new SpaceXDeatailsModel;
           launchDetails.mission_name = val.mission_name;
@@ -85,8 +104,9 @@ landsuccess : string ="";
           launchDetails.mission_id = val.mission_id;
           launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
           this.launchDetailsList.push(launchDetails);
-          
+
         }
+      }
 
       },error =>{
       }
@@ -96,7 +116,12 @@ landsuccess : string ="";
   getLaunchyearDetails(launchyer_filter : string){
     this.service.getAllLaunch_yearDetails("100",launchyer_filter).subscribe(
       res =>{
+        this.nullCheck=false;
         this.launchDetailsList=[];
+        if(res.length === 0){
+          this.listNullCheck();
+        }
+        if(res !== null){
         for(let val of res){
           const launchDetails = new SpaceXDeatailsModel;
           launchDetails.mission_name = val.mission_name;
@@ -107,8 +132,9 @@ landsuccess : string ="";
           launchDetails.mission_id = val.mission_id;
           launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
           this.launchDetailsList.push(launchDetails);
-          
+
         }
+      }
 
       },error =>{
       }
@@ -120,19 +146,27 @@ landsuccess : string ="";
   getAllFilters(launch_sucess_filter : string,land_success_filter : string,launch_year_filter : string){
     this.service.getAllFilters("100",launch_sucess_filter,land_success_filter,launch_year_filter).subscribe(
       res =>{
+        this.nullCheck=false;
         this.launchDetailsList=[];
-        for(let val of res){
-          const launchDetails = new SpaceXDeatailsModel;
-          launchDetails.mission_name = val.mission_name;
-          launchDetails.flight_number = val.flight_number;
-          launchDetails.launch_success = val.launch_success;
-          launchDetails.links.mission_patch = val.links.mission_patch;
-          launchDetails.launch_year = val.launch_year;
-          launchDetails.mission_id = val.mission_id;
-          launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
-          this.launchDetailsList.push(launchDetails);
-          
+
+        if(res.length === 0){
+          this.listNullCheck();
         }
+        if(res !== null){
+          for(let val of res){
+            const launchDetails = new SpaceXDeatailsModel;
+            launchDetails.mission_name = val.mission_name;
+            launchDetails.flight_number = val.flight_number;
+            launchDetails.launch_success = val.launch_success;
+            launchDetails.links.mission_patch = val.links.mission_patch;
+            launchDetails.launch_year = val.launch_year;
+            launchDetails.mission_id = val.mission_id;
+            launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
+            this.launchDetailsList.push(launchDetails);
+
+          }
+        }
+        
 
       },error =>{
       }
@@ -143,7 +177,12 @@ landsuccess : string ="";
   getLaunchAndLandFilter(launch_sucess_filter : string,land_success_filter : string){
     this.service.getLaunchAndLandFilter("100",launch_sucess_filter,land_success_filter).subscribe(
       res =>{
+        this.nullCheck=false;
         this.launchDetailsList=[];
+        if(res.length === 0){
+          this.listNullCheck();
+        }
+        if(res !== null){
         for(let val of res){
           const launchDetails = new SpaceXDeatailsModel;
           launchDetails.mission_name = val.mission_name;
@@ -154,9 +193,9 @@ landsuccess : string ="";
           launchDetails.mission_id = val.mission_id;
           launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
           this.launchDetailsList.push(launchDetails);
-          
-        }
 
+        }
+      }
       },error =>{
       }
     )
@@ -165,7 +204,12 @@ landsuccess : string ="";
   getLaunchAndyearFilter(launch_sucess_filter : string,launch_year_filter : string){
     this.service.getLaunchAndyearFilter("100",launch_sucess_filter,launch_year_filter).subscribe(
       res =>{
+        this.nullCheck=false;
         this.launchDetailsList=[];
+        if(res.length === 0){
+          this.listNullCheck();
+        }
+        if(res !== null){
         for(let val of res){
           const launchDetails = new SpaceXDeatailsModel;
           launchDetails.mission_name = val.mission_name;
@@ -176,8 +220,9 @@ landsuccess : string ="";
           launchDetails.mission_id = val.mission_id;
           launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
           this.launchDetailsList.push(launchDetails);
-          
+
         }
+      }
 
       },error =>{
       }
@@ -187,7 +232,12 @@ landsuccess : string ="";
   getyearAndLandFilter(launch_year_filter : string,land_success_filter : string){
     this.service.getyearAndLandFilter("100",launch_year_filter,land_success_filter).subscribe(
       res =>{
+        this.nullCheck=false;
+        if(res.length === 0){
+          this.listNullCheck();
+        }
         this.launchDetailsList=[];
+        if(res !== null){
         for(let val of res){
           const launchDetails = new SpaceXDeatailsModel;
           launchDetails.mission_name = val.mission_name;
@@ -198,9 +248,9 @@ landsuccess : string ="";
           launchDetails.mission_id = val.mission_id;
           launchDetails.land_success=val.rocket.first_stage.cores[0].land_success
           this.launchDetailsList.push(launchDetails);
-          
-        }
 
+        }
+      }
       },error =>{
       }
     )
@@ -214,28 +264,28 @@ landsuccess : string ="";
     }
     document.getElementById(val).classList.add('active')
 
-    if(this.launchyear!="" && this.landsuccess != "" && this.launchsuccess != ""){
+    if(this.launchyear!=="" && this.landsuccess !== "" && this.launchsuccess !== ""){
       this.getAllFilters(this.launchsuccess,this.landsuccess,this.launchyear);
 
     }
-    else if(this.launchsuccess== "" && this.landsuccess != "" && this.launchyear != ""){
+    else if(this.launchsuccess=== "" && this.landsuccess !== "" && this.launchyear !== ""){
       this.getyearAndLandFilter(this.launchyear,this.landsuccess);
 
     }
 
-    else if(this.landsuccess == "" && this.launchsuccess != "" && this.launchyear != ""){
+    else if(this.landsuccess === "" && this.launchsuccess !== "" && this.launchyear !== ""){
       this.getLaunchAndyearFilter(this.launchsuccess,this.launchyear);
     }
-      else if(this.landsuccess == "" && this.launchsuccess == "" && this.launchyear != ""){
+      else if(this.landsuccess === "" && this.launchsuccess === "" && this.launchyear !== ""){
         this.getLaunchyearDetails(this.launchyear);
       }
   }
 
   SucessfullLaunch(val: string){
-    if(val == "True"){
+    if(val === "True"){
       this.launchsuccess = "true"
     }
-    else if(val == "False"){
+    else if(val === "False"){
       this.launchsuccess = "false"
     }
     
@@ -245,30 +295,30 @@ landsuccess : string ="";
 
     document.getElementById(val).classList.add('active');
 
-    if(this.launchyear!="" && this.landsuccess !="" && this.launchsuccess !=""){
+    if(this.launchyear!=="" && this.landsuccess !=="" && this.launchsuccess !==""){
       this.getAllFilters(this.launchsuccess,this.landsuccess,this.launchyear);
     }
 
 
-    else if(this.landsuccess!="" && this.landsuccess !="" && this.launchyear ==""){
+    else if(this.landsuccess!=="" && this.landsuccess !=="" && this.launchyear ===""){
       this.getLaunchAndLandFilter(this.launchsuccess,this.landsuccess);
   }
 
 
-    else if(this.landsuccess == "" && this.launchsuccess != "" && this.launchyear == ""){
+    else if(this.landsuccess === "" && this.launchsuccess !== "" && this.launchyear === ""){
       this.getLaunchSuccessDetails(this.launchsuccess);
 
     }
-      else if(this.landsuccess == "" && this.launchsuccess != "" && this.launchyear != ""){
+      else if(this.landsuccess === "" && this.launchsuccess !== "" && this.launchyear !== ""){
         this.getLaunchAndyearFilter(this.launchsuccess,this.launchyear);
       }
   }
 
   SucessfullLanding(val : string){
-    if(val == "True"){
+    if(val === "True"){
       this.landsuccess = "true"
     }
-    else if(val == "False"){
+    else if(val === "False"){
       this.landsuccess = "false"
     }
     
@@ -278,20 +328,28 @@ landsuccess : string ="";
     }
     document.getElementById("landing" + val).classList.add('active');
 
-    if(this.launchyear!="" && this.landsuccess != "" && this.launchsuccess != ""){
+    if(this.launchyear!=="" && this.landsuccess !== "" && this.launchsuccess !== ""){
       this.getAllFilters(this.launchsuccess,this.landsuccess,this.launchyear);
 
     }
-    else if(this.landsuccess!= "" && this.launchsuccess != "" && this.launchyear == ""){
+    else if(this.landsuccess!== "" && this.launchsuccess !== "" && this.launchyear === ""){
       this.getLaunchAndLandFilter(this.launchsuccess,this.landsuccess);
 
     }
 
-    else if(this.landsuccess != "" && this.launchsuccess == "" && this.launchyear == ""){
+    else if(this.landsuccess !== "" && this.launchsuccess === "" && this.launchyear === ""){
       this.getLandSuccessDetails(this.landsuccess);
     }
-      else if(this.landsuccess != "" && this.launchsuccess == "" && this.launchyear != ""){
+      else if(this.landsuccess !== "" && this.launchsuccess === "" && this.launchyear !== ""){
         this.getyearAndLandFilter(this.launchyear,this.landsuccess);
       }
+  }
+
+  listNullCheck(){
+    this.nullCheck=false;
+    if(this.launchDetailsList === null || this.launchDetailsList.length===0){
+      this.nullCheck=true;
+
+    }
   }
 } 
